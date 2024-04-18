6863ee27a72657cb0f657f36ee41d2827f2dfb04
Mjacm15 <alfamar2005@gmail.com>
2024-04-17 20:44:55 -0400

    details page

--- details.php
+++ details.php
@@ -1,15 +1,30 @@
 
+<?php
+    require_once('njit_database.php');
+    $db = getDB();
+    
+    if(isset($_GET['accessoryID'])) {
+        // Sanitize the input to prevent SQL injection using prepared statements
+        $product_id = $_GET['accessoryID'];
+        
+    }
 
+    $query = 'SELECT * FROM accessories WHERE accessoryID = :accessoryID';
+    $statement1=$db->prepare($query);
+    $statement1->bindValue('accessoryID', $product_id);
+    $statement1->execute();
+    $productDetails=$statement1->fetch(PDO::FETCH_ASSOC);
+    $statement1->closeCursor();
+
+    
+
+
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,18 +36,82 @@
     <script src="delete.js"></script>
 
 </head>
-<main>
 
-</main>
-<footer>
-        <h1>Sip and Stir</h1>
-        <address>
-            Sip & Stir<br>
-            21 Brick Ave<br>
-            Newark, NJ, 04894<br>
-        </address>
-        <h5>@ Sip and Stir, 2024</h5>
+<body>
+<header>
+        <h1>Sip & Stir</h1>
+        <?php
+        //welcome message
+
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
+        if(isset($_SESSION['is_valid_admin']) && $_SESSION['is_valid_admin']){
+        $email=$_SESSION['email'];
+        $firstName=$_SESSION['firstName'];
+        $lastName=$_SESSION['lastName'];
+        echo "<h2 id='welcome_message'> Welcome $firstName $lastName ($email)</h2>";
+        }
+        ?>
+        <nav class="mainNav">
+            <a href="home.php">Home</a> |
+            <a href="product.php">Products</a> 
+
+            <?php
+               if(!isset($_SESSION['is_valid_admin'])){
+                
+                echo '<a class="log"  href="login.php">log in</a>';
+               }else{
+                echo '| <a href="shipping.php">Shipping</a> |';
+                echo '<a href="create.php">Create</a>';
+                echo '<a class="log" href="logout.php">log out</a>';
+               }
+
+            ?>
+        </nav>
+        <!-- //////////////////////////////////dont forgwt -->
+        <h2 id="product_name"><?php echo $productDetails['accessoryName']?></h2>
+
         
-    </footer> 
+     
+        
+
+
+
+    </header>
+    <main>
+        <?php
+            if(count($productDetails)===0){
+                echo "<br>";
+                echo "<p>Error: Accessory doesn't not exist<p>";
+                echo "<br>";
+                echo "<a href='product.php'>Go to Product List</a>";
+            }  
+            
+            
+            foreach ($productDetails as $key => $value) {
+                echo "$key: $value<br>";
+            }
+        ?>
+
+        
+
+        <!-- <p>Accessory Name: <?php echo $productDetails['accessoryName']?></p>
+        <p>Accessory Code: <?php echo $productDetails['accessoryCode']?></p>
+        <p>Accessory Code: <?php echo $productDetails['accessoryCode']?></p> -->
+
+
+    </main>
+    <footer>
+            <h1>Sip and Stir</h1>
+            <address>
+                Sip & Stir<br>
+                21 Brick Ave<br>
+                Newark, NJ, 04894<br>
+            </address>
+            <h5>@ Sip and Stir, 2024</h5>
+            
+        </footer> 
 </body>
 </html>
